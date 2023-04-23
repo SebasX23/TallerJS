6b62c15ac003dc6f79c88393ca08cfa8cc37baec
SebasX23 <sebastianosp07@gmail.com>
2023-04-23 00:56:43 -0500

    Taller Finalizado

--- punto10.js
+++ punto10.js
@@ -1,7 +1,3 @@
-//Crear una función llamada "reemplazarLetras" que tome un string y dos letras como parámetros. La
-//función debe reemplazar todas las apariciones de la primera letra en el string por la segunda letra y
-//devolver el string modificado. Luego, crear una variable "mensaje" con un string y llamar a la función
-//con el mensaje, la letra "a" y la letra "o" como parámetros. Imprimir en la consola el string modificado.
 function reemplazarLetras(mensaje, letra1, letra2) {
     let nuevoMensaje = mensaje
     for (let i = 0; i < nuevoMensaje.length; i++) {
